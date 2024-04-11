cd40bffd0fd4a9aabe56700e427090d9163679ec
Isa01709 <isaaclee2014@gmail.com>
2024-04-11 16:04:23 +0100

    Grid array creation.

--- Tic-Tac-Toe/Class1.cs
+++ Tic-Tac-Toe/Class1.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Text;
 
 namespace Tic_Tac_Toe
 {
-    public class Class1
+    class program
     {
+        //Making the array of spots on the grid.
+        static char[] array = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
     }
 }
