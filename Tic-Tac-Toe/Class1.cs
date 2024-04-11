0c0deb96f53e8341d768addac37217e8234cb00c
Isa01709 <isaaclee2014@gmail.com>
2024-04-11 17:23:20 +0100

    Fixed missing parentheses.

--- Tic-Tac-Toe/Class1.cs
+++ Tic-Tac-Toe/Class1.cs
@@ -9,13 +9,14 @@ namespace Tic_Tac_Toe
     class program
     {
         //Making the array of spots on the grid, and the 'choice' of position made by the specified user.
-        static char[] gridArray = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        static char[] gridArray = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
         static int playerChoice;
         static int player = 1;
         static int flag = 0;
 
         static void Main(string[] args)
         {
+            int status;
             do
             {
                 Console.Clear(); // When the game starts/restarts, the program is cleared.
@@ -72,13 +73,13 @@ namespace Tic_Tac_Toe
         private static void Grid()
         {
             Console.WriteLine("     |     |      ");
-            Console.WriteLine($"  {gridArray[1]}  |  {gridArray[2]}  |  {gridArray[3]}";
+            Console.WriteLine($"  {gridArray[1]}  |  {gridArray[2]}  |  {gridArray[3]}");
             Console.WriteLine("_____|_____|_____ ");
             Console.WriteLine("     |     |      ");
-            Console.WriteLine($"  {gridArray[4]}  |  {gridArray[5]}  |  {gridArray[6]}";
+            Console.WriteLine($"  {gridArray[4]}  |  {gridArray[5]}  |  {gridArray[6]}");
             Console.WriteLine("_____|_____|_____ ");
             Console.WriteLine("     |     |      ");
-            Console.WriteLine($"  {gridArray[7]}  |  {gridArray[8]}  |  {gridArray[9]}";
+            Console.WriteLine($"  {gridArray[7]}  |  {gridArray[8]}  |  {gridArray[9]}");
             Console.WriteLine("     |     |      ");
         }
         //Checks game status.
